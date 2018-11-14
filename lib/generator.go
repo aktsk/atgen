b90e2198dbaa2596b18f88dd2ce8bc058aa9d678
Gosuke Miyashita <gosukenator@gmail.com>
2018-11-14 17:04:38 +0900

    temporary commit

--- lib/generator.go
+++ lib/generator.go
@@ -21,47 +21,93 @@ func (g *Generator) Generate() error {
 
 	var (
 		testFuncNode ast.Node
-		//		testNode     ast.Node
-		//		subTestsNode ast.Node
+		testNode     ast.Node
+		subtestNode  ast.Node
 	)
 
 	cmap := ast.NewCommentMap(fset, f, f.Comments)
 	for node, cgroups := range cmap {
 		for _, cgroup := range cgroups {
-			if strings.Contains(cgroup.Text(), "Begin TestFunc") {
+			if strings.Contains(cgroup.Text(), "TestFunc block") {
 				testFuncNode = node
+			} else if strings.Contains(cgroup.Text(), "Test block") {
+				testNode = node
+			} else if strings.Contains(cgroup.Text(), "Subtest block") {
+				subtestNode = node
 			}
 		}
 	}
 
-	var nodes []ast.Node
-	for _, testFunc := range g.TestFuncs {
-		n := util.DuplicateNode(testFuncNode)
-		n.(*ast.FuncDecl).Name.Name = testFunc.Name
-		for _, test := range testFunc.Tests {
-			_ = test
-			//fmt.Printf("%#v\n", test)
-			//fmt.Println(test.IsSubtests())
+	astutil.Apply(testFuncNode, func(cr *astutil.Cursor) bool {
+		if cr.Node() == testNode {
+			cr.Delete()
 		}
-		nodes = append(nodes, n)
-	}
-
-	n := astutil.Apply(f, func(cr *astutil.Cursor) bool {
-		if cr.Name() == "Decls" {
-			switch cr.Node().(type) {
-			case *ast.FuncDecl:
-				for _, n := range nodes {
-					cr.InsertBefore(n)
-				}
-			}
-		}
-		if cr.Node() == testFuncNode {
+		if cr.Node() == subtestNode {
 			cr.Delete()
 		}
 		return true
 	}, nil)
 
-	printer.Fprint(os.Stdout, fset, n)
+	var tfnodes []ast.Node
+	for _, testFunc := range g.TestFuncs {
+		tfnode := util.DuplicateNode(testFuncNode)
+		tfnode.(*ast.FuncDecl).Name.Name = testFunc.Name
+
+		var tnodes []ast.Node
+		for _, test := range testFunc.Tests {
+			var tnode ast.Node
+			if !test.IsSubtests() {
+				tnode = util.DuplicateNode(testNode)
+				tnode = rewriteTestNode(tnode, test.(Test))
+			}
+			tnodes = append(tnodes, tnode)
+
+		}
+
+		astutil.Apply(tfnode, func(cr *astutil.Cursor) bool {
+			switch v := cr.Node().(type) {
+			case *ast.BlockStmt:
+				if v.List == nil {
+					for _, n := range tnodes {
+						cr.InsertBefore(n)
+					}
+					cr.Delete()
+				}
+			}
+
+			return true
+		}, nil)
+
+		tfnodes = append(tfnodes, tfnode)
+	}
+
+	astutil.Apply(f, func(cr *astutil.Cursor) bool {
+		if cr.Name() == "Decls" {
+			switch cr.Node().(type) {
+			case *ast.FuncDecl:
+				for _, n := range tfnodes {
+					cr.InsertBefore(n)
+				}
+			}
+		}
+
+		if cr.Node() == testFuncNode {
+			cr.Delete()
+		}
+
+		return true
+	}, nil)
+
+	f.Comments = cmap.Filter(f).Comments()
+	printer.Fprint(os.Stdout, fset, f)
 
 	return nil
+}
+
+func rewriteTestNode(n ast.Node, test Test) ast.Node {
+	astutil.Apply(n, func(cr *astutil.Cursor) bool {
+		// TODO: rewrite test node
+		return true
+	}, nil)
+	return n
 }
