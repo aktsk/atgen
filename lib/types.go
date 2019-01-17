629aecbc38f6094d69739aa8d78e01858a4a51b0
Gosuke Miyashita <gosukenator@gmail.com>
2019-01-17 10:54:52 +0900

    Add comments for golint

--- lib/types.go
+++ lib/types.go
@@ -10,8 +10,10 @@ type Generator struct {
 	TestFuncsPerAPIVersion map[string]TestFuncs
 }
 
+// TestFuncs is a group of TestFunc
 type TestFuncs []TestFunc
 
+// TestFunc represents a test function
 type TestFunc struct {
 	Name        string
 	Tests       []Tester
@@ -19,6 +21,7 @@ type TestFunc struct {
 	Vars        map[string]interface{}
 }
 
+// Test represents a test in a test function
 type Test struct {
 	APIVersions []string
 	Path        string
@@ -28,33 +31,40 @@ type Test struct {
 	Vars        map[string]interface{}
 }
 
+// Subtests is a group of Subtest
 type Subtests []Subtest
 
+// Subtest reppresents a subtest
 type Subtest struct {
 	Name        string
 	Tests       []Test
 	APIVersions []string
 }
 
+// Req is a request parameters and headers which a test should throw
 type Req struct {
 	Params  map[string]interface{}
 	Headers map[string]string
 }
 
+// Res is a response status, parameters and headers which a test should get
 type Res struct {
 	Status  int
 	Params  map[string]interface{}
 	Headers map[string]string
 }
 
+// Tester is an interface for Test and Subtest
 type Tester interface {
 	IsSubtests() bool
 }
 
+// IsSubtests returns false when t is Test
 func (t Test) IsSubtests() bool {
 	return false
 }
 
+// IsSubtests returns true when t is Subtests
 func (t Subtests) IsSubtests() bool {
 	return true
 }
