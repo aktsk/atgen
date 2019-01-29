0e602486fe64bd28c45ef0c9dce262d30447e12f
Takumasa Sakao <takumasa.sakao@aktsk.jp>
2019-01-29 13:58:29 +0900

    Implement DummyRouter for example

--- example/handlers/handler.go
+++ example/handlers/handler.go
@@ -1,9 +1,14 @@
 package handlers
 
-import "github.com/gorilla/mux"
+import (
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
 
 // DummyRouter is router for test
 func DummyRouter() *mux.Router {
 	router := mux.NewRouter()
+	router.HandleFunc("/v1/", func(w http.ResponseWriter, r *http.Request) {})
 	return router
 }
